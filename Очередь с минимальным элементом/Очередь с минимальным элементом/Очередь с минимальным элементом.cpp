761ee1d8bf88e76ae69e1344cc3733e0348bc8d4
EgorKudryashov <70726535+EgorKudryashov@users.noreply.github.com>
2021-12-31 18:57:26 +0300

    Нахождение минимального элемента в очереди
    Простой вариант с использование одной очереди

---
+++ Очередь с минимальным элементом/Очередь с минимальным элементом/Очередь с минимальным элементом.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <fstream>
+
+double* pop(double*& A, int& n) {
+	double* B = new double[n + 10];
+	for (int i = 0; i < n; ++i) {
+		B[i] = A[i];
+	}
+	n += 10;
+	delete[] A;
+	return B;
+}
+
+void search_min(double*& A, int n, std::ofstream& out) {
+	double min = A[0];
+	for (int i = 1; i < n; ++i) {
+		if (A[i] < min) {
+			min = A[i];
+		}
+	}
+	out << min;
+	out << '\n';
+}
+
+int main()
+{
+	std::ifstream inFile;
+	inFile.open("input.txt");
+	if (!inFile) {
+		std::cout << "Don't open file for writing";
+		return 0;
+	}
+	int n, coll = 0;
+	double tmp;
+	char sym;
+	inFile >> n;
+	int range = n / 2;
+	double* stek = new double[range];
+	std::ofstream outFile;
+	outFile.open("output.txt");
+	if (!outFile) {
+		std::cout << "Don't open file";
+		return 0;
+	}
+	for (int i = 0; i < n; ++i) {
+		inFile >> sym;
+		switch (sym) {
+		case '+': {
+			inFile >> tmp;
+			++coll;
+			if (coll > range) {
+				stek = pop(stek, range);
+			}
+			stek[coll - 1] = tmp;
+			break;
+		}
+		case '-': {
+			--coll;
+			break;
+		}
+		case '?': {
+			search_min(stek, coll, outFile);
+			break;
+		}
+		}
+	}
+
+	outFile.close();
+	inFile.close();
+	delete[] stek;
+	return 0;
+}
